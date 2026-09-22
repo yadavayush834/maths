19fb5c3345a497c6b6abb6d7197c01f9b7b4d0ea
yadavayush834 <ayushwell100@gmail.com>
2026-09-22 22:45:05 +0530

    Update math exercises

--- day-01/q02-sum-multiples-k-interval/sol.cpp
+++ day-01/q02-sum-multiples-k-interval/sol.cpp
@@ -1,15 +1,13 @@
-// sum multiples of k in [L, R], no loop
-// trick: sum up to N is k * m * (m + 1) / 2 where m = N / k
-// ans = sum(R) - sum(L - 1)
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-  long long L, R, k;
-  if (!(cin >> L >> R >> k)) return 0;
-  long long a = R / k;
-  long long b = (L - 1) / k;
-  long long ans = k * (a * (a + 1) / 2 - b * (b + 1) / 2);
-  cout << ans;
-  return 0;
+
+int main(){
+  int L,R, k ;
+  cin >> L>> R >> k ; 
+
+  int count = (R-L)/k + 1 ;
+  int ans  = count *(R+L) / 2 ; 
+  cout << ans << endl; 
 }
